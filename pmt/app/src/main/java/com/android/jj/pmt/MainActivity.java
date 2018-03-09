91b63abfbf8c43591c62c665f01c5124e34d9e69
jingjing.wang <jingjing.wang@tinno.com>
2018-03-09 13:57:10 +0800

    add code about test PARTIAL_WAKE_LOCK

--- pmt/app/src/main/java/com/android/jj/pmt/MainActivity.java
+++ pmt/app/src/main/java/com/android/jj/pmt/MainActivity.java
@@ -1,10 +1,21 @@
 package com.android.jj.pmt;
 
+import android.content.Context;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "pmt";
+
+    private Button mButtonPARTIALWL;
+    //true means get PARTIAL wakelock
+    private boolean mbGetPARTIALWL = false;
+    private PowerManager.WakeLock mWakeLockPARTIAL = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
     tv.setText(stringFromJNI());
+
+        mButtonPARTIALWL = (Button)findViewById(R.id.button_id_PARTIAL_wakelock);
     }
 
     /**
@@ -25,5 +38,29 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
+    }
+
+    public void button_click_PARTIAL_wakelock(View v) {
+        Log.w(TAG, "button_click_PARTIAL_wakelock: mbGetPARTIALWL=" + mbGetPARTIALWL);
+        if (mbGetPARTIALWL == false) {
+            mbGetPARTIALWL = !mbGetPARTIALWL;
+            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            mWakeLockPARTIAL = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+            if (mWakeLockPARTIAL != null) {
+                mWakeLockPARTIAL.acquire();
+            }
+            mButtonPARTIALWL.setText("get PARTIAL wl");
+        } else {
+            mbGetPARTIALWL = !mbGetPARTIALWL;
+            release_PARTIAL_wakelock();
+            mButtonPARTIALWL.setText("release PARTIAL wl");
+        }
+    }
+
+    private void release_PARTIAL_wakelock() {
+        if (mWakeLockPARTIAL != null) {
+            mWakeLockPARTIAL.release();
+            mWakeLockPARTIAL = null;
+        }
     }
 }
